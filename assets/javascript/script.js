34e0f8ba1f10072a3039e00643f0bf582c2a78df
Brian Zauner <binrz00@hotmail.com>
2019-11-23 00:36:52 -0600

    added axios, set up url's,grabbed data from api requests

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,14 +1,38 @@
 //set up weather api url
+const city = "forest lake"//user input
+const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=82e0e6ab873a6ac39caada2c8b0fafd5"
 // axios get weather
+axios.get(url).then(function(response){
+    console.log(response);
 //json parse the data requested
 //display under current weather conditions (maybe need moment)
 //* City
+const town = response.data.name;
+console.log(town);
 //* Date
+let myDate = new Date(response.data.dt*1000);
+console.log(myDate);
 //* Icon image (visual representation of weather conditions)
+const icon = response.data.weather.icon;
+console.log(icon);
 //* Temperature
+const tempKelvin = response.data.main.temp;
+console.log(tempKelvin);
 //* Humidity
+const humidity = response.data.main.humidity;
+console.log(humidity);
 //* Wind speed
+const windSpeed = response.data.wind.speed;
+console.log(windSpeed);
 //* UV index
+const lat = response.data.coord.lat;
+const lon = response.data.coord.lon;
+const uvIndexurl = "https://api.openweathermap.org/data/2.5/uvi?appid=82e0e6ab873a6ac39caada2c8b0fafd5&lat="+lat+"&lon="+lon;
+axios.get(uvIndexurl).then(function(responseUv){
+    const uvIndex = responseUv;
+    console.log(uvIndex);
+    console.log(responseUv.data.value);
+})
 //search history with city name (local storage)
 // clicking on the city name gets current and future data 
 //5 day forcast under current weather with:
@@ -16,3 +40,4 @@
 //* Icon image (visual representation of weather conditions)
 //* Temperature
   //Humidity
+})
