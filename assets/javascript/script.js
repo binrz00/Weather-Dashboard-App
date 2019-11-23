180e2ad209edcf075b45058a4354e05b31e6c0d9
Brian Zauner <binrz00@hotmail.com>
2019-11-22 22:18:27 -0600

    psuedo code

---
+++ assets/javascript/script.js
@@ -0,0 +1,18 @@
+//set up weather api url
+// axios get weather
+//json parse the data requested
+//display under current weather conditions (maybe need moment)
+//* City
+//* Date
+//* Icon image (visual representation of weather conditions)
+//* Temperature
+//* Humidity
+//* Wind speed
+//* UV index
+//search history with city name (local storage)
+// clicking on the city name gets current and future data 
+//5 day forcast under current weather with:
+//* Date
+//* Icon image (visual representation of weather conditions)
+//* Temperature
+  //Humidity
