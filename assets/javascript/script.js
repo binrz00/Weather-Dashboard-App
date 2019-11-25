8b0991ae26f26069106adc706be1b05ad7b6f843
Brian Zauner <binrz00@hotmail.com>
2019-11-24 21:17:51 -0600

    added areas to display data, added styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,5 @@
 //set up weather api url
-const city = "forest lake"//user input
+/*const city = "forest lake"//user input
 const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +"&units=imperial&appid=82e0e6ab873a6ac39caada2c8b0fafd5"
 // axios get weather
 axios.get(url).then(function(response){
@@ -8,30 +8,30 @@ axios.get(url).then(function(response){
 //display under current weather conditions (maybe need moment)
 //* City
 const town = response.data.name;
-console.log(town);
+console.log("town:" +town);
 //* Date
 let myDate = new Date(response.data.dt*1000);
-console.log(myDate);
+console.log("date: "+myDate);
 //* Icon image (visual representation of weather conditions)
 const icon = response.data.weather.icon;
-console.log(icon);
+console.log("icon:"+icon);
 //* Temperature
-const tempKelvin = response.data.main.temp;
-console.log(tempKelvin);
+const temp = response.data.main.temp;
+console.log("temp:"+temp);
 //* Humidity
 const humidity = response.data.main.humidity;
-console.log(humidity);
+console.log("humidity:"+humidity);
 //* Wind speed
 const windSpeed = response.data.wind.speed;
-console.log(windSpeed);
+console.log("wind:"+windSpeed);
 //* UV index
 const lat = response.data.coord.lat;
 const lon = response.data.coord.lon;
 const uvIndexurl = "https://api.openweathermap.org/data/2.5/uvi?appid=82e0e6ab873a6ac39caada2c8b0fafd5&lat="+lat+"&lon="+lon;
 axios.get(uvIndexurl).then(function(responseUv){
     const uvIndex = responseUv;
-    console.log(uvIndex);
-    console.log(responseUv.data.value);
+    //console.log(uvIndex);
+    console.log("uv index:"+responseUv.data.value);
 })
 //search history with city name (local storage)
 // clicking on the city name gets current and future data 
@@ -40,4 +40,4 @@ axios.get(uvIndexurl).then(function(responseUv){
 //* Icon image (visual representation of weather conditions)
 //* Temperature
   //Humidity
-})
+})*/
