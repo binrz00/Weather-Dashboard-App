83cc5bfa1aaa70872f2e8c205235341bb98a3dbc
Brian Zauner <binrz00@hotmail.com>
2019-11-26 21:01:08 -0600

    dryed up code.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,9 @@
-
+let data = JSON.parse(localStorage.getItem("key"));
+if (data.length > 0) {
+  weather(data[0]);
+}
 document.getElementById("citySearch").addEventListener("click", function () {
   let city = document.getElementById("search").value;
-
   createButton(city);
   weather(city);
   document.getElementById("search").value = "";
@@ -9,13 +11,11 @@ document.getElementById("citySearch").addEventListener("click", function () {
 function weather(city) {
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=82e0e6ab873a6ac39caada2c8b0fafd5"
   axios.get(url).then(function (response) {
-
     const icon = response.data.weather[0].icon;
     document.getElementById("icon").setAttribute("src", "http://openweathermap.org/img/wn/" + icon + ".png")
     const theDate = new Date(response.data.dt * 1000).toLocaleDateString().split(",")[0];
     const town = response.data.name;
     document.getElementById("city").innerHTML = town + " (" + theDate + ")";
-
     const temp = response.data.main.temp;
     document.getElementById("temp").innerHTML = "Temperature: " + temp + "℉";
     const humidity = response.data.main.humidity;
@@ -29,51 +29,16 @@ function weather(city) {
       document.getElementById("uv").innerHTML = "UV index: " + responseUv.data.value;
       const fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?appid=82e0e6ab873a6ac39caada2c8b0fafd5&lat=" + lat + "&lon=" + lon + "&units=imperial&";
       axios.get(fiveDayUrl).then(function (responseFive) {
-
-        const date1 = new Date(responseFive.data.list[2].dt * 1000).toLocaleDateString().split(",")[0];
-        document.getElementById("date1").innerHTML = date1
-        const icon1 = responseFive.data.list[2].weather[0].icon;
-        document.getElementById("icon1").setAttribute("src", "http://openweathermap.org/img/wn/" + icon1 + ".png");
-        const temp1 = responseFive.data.list[2].main.temp;
-        document.getElementById("temp1").innerHTML = "Temp: " + temp1 + "℉";
-        const humidity1 = responseFive.data.list[2].main.humidity;
-        document.getElementById("humid1").innerHTML = "Humidity: " + humidity1 + "%";
-
-        const date2 = new Date(responseFive.data.list[10].dt * 1000).toLocaleDateString().split(",")[0];
-        document.getElementById("date2").innerHTML = date2
-        const icon2 = responseFive.data.list[10].weather[0].icon;
-        document.getElementById("icon2").setAttribute("src", "http://openweathermap.org/img/wn/" + icon2 + ".png");
-        const temp2 = responseFive.data.list[10].main.temp;
-        document.getElementById("temp2").innerHTML = "Temp: " + temp2 + "℉";
-        const humidity2 = responseFive.data.list[10].main.humidity;
-        document.getElementById("humid2").innerHTML = "Humidity: " + humidity2 + "%";
-
-        const date3 = new Date(responseFive.data.list[18].dt * 1000).toLocaleDateString().split(",")[0];
-        document.getElementById("date3").innerHTML = date3
-        const icon3 = responseFive.data.list[18].weather[0].icon;
-        document.getElementById("icon3").setAttribute("src", "http://openweathermap.org/img/wn/" + icon3 + ".png");
-        const temp3 = responseFive.data.list[18].main.temp;
-        document.getElementById("temp3").innerHTML = "Temp: " + temp3 + "℉";
-        const humidity3 = responseFive.data.list[18].main.humidity;
-        document.getElementById("humid3").innerHTML = "Humidity: " + humidity3 + "%";
-
-        const date4 = new Date(responseFive.data.list[26].dt * 1000).toLocaleDateString().split(",")[0];
-        document.getElementById("date4").innerHTML = date4
-        const icon4 = responseFive.data.list[26].weather[0].icon;
-        document.getElementById("icon4").setAttribute("src", "http://openweathermap.org/img/wn/" + icon4 + ".png");
-        const temp4 = responseFive.data.list[26].main.temp;
-        document.getElementById("temp4").innerHTML = "Temp: " + temp4 + "℉";
-        const humidity4 = responseFive.data.list[26].main.humidity;
-        document.getElementById("humid4").innerHTML = "Humidity: " + humidity4 + "%";
-
-        const date5 = new Date(responseFive.data.list[34].dt * 1000).toLocaleDateString().split(",")[0];
-        document.getElementById("date5").innerHTML = date5
-        const icon5 = responseFive.data.list[34].weather[0].icon;
-        document.getElementById("icon5").setAttribute("src", "http://openweathermap.org/img/wn/" + icon5 + ".png");
-        const temp5 = responseFive.data.list[34].main.temp;
-        document.getElementById("temp5").innerHTML = "Temp: " + temp5 + "℉";
-        const humidity5 = responseFive.data.list[34].main.humidity;
-        document.getElementById("humid5").innerHTML = "Humidity: " + humidity5 + "%";
+        for (let i = 0; i < responseFive.data.list.length; i++) {
+          const date = new Date(responseFive.data.list[i * 8 + 2].dt * 1000).toLocaleDateString().split(",")[0];
+          document.getElementById("date" + i).innerHTML = date;
+          const icon = responseFive.data.list[i * 8 + 2].weather[0].icon;
+          document.getElementById("icon" + i).setAttribute("src", "http://openweathermap.org/img/wn/" + icon + ".png");
+          const temp = responseFive.data.list[i * 8 + 2].main.temp;
+          document.getElementById("temp" + i).innerHTML = "Temp: " + temp + "℉";
+          const humidity = responseFive.data.list[i * 8 + 2].main.humidity;
+          document.getElementById("humid" + i).innerHTML = "Humidity: " + humidity + "%";
+        }
       })
     })
   })
@@ -81,17 +46,17 @@ function weather(city) {
 function createButton(city) {
   let prevData = localStorage.getItem("key") || "[]";
   let data = JSON.parse(prevData);
-  data.push(city);
-  if(data.length>5){
-    data.shift();}
-    localStorage.setItem("key", JSON.stringify(data));
-    const buttons = document.querySelectorAll(".cities");
-      console.log(buttons);
-      buttons.forEach(function(town){
-        town.classList.add("d-none");
-      })
+  data.unshift(city);
+  if (data.length > 5) {
+    data.pop();
+  }
+  localStorage.setItem("key", JSON.stringify(data));
+  const buttons = document.querySelectorAll(".cities");
+  buttons.forEach(function (town) {
+    town.classList.add("d-none");
+  })
   data.forEach(town => {
-let button = document.createElement("div");
+    let button = document.createElement("div");
     button.classList.add("cities");
     button.classList.add("rounded-sm");
     console.log(button);
@@ -104,6 +69,3 @@ let button = document.createElement("div");
     document.getElementById("cities").append(button)
   });
 }
-
-
-
